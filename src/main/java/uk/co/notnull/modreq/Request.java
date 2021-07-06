f1357e42348eef692bb26b24c5bc580304265f21
James Lyne <jim+github@not-null.co.uk>
2021-07-06 23:33:41 +0100

    Copyright headers

--- src/main/java/uk/co/notnull/modreq/Request.java
+++ src/main/java/uk/co/notnull/modreq/Request.java
@@ -1,3 +1,25 @@
+/*
+ * ModReq
+ * Copyright (C) 2021 James Lyne
+ *
+ * Based on ModReq 1.2 (https://www.spigotmc.org/resources/modreq.57560/)
+ * Copyright (C) 2019 Aladram and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package uk.co.notnull.modreq;
 
 import de.themoep.minedown.adventure.MineDownParser;
